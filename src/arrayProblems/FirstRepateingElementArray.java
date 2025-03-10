cd75e5bd3508f27b22703284100bc313eaf29410
nehalahmeed1 <nehalahmed47@gmail.com>
2025-03-10 15:40:43 +0530

    Initial commit

---
+++ src/arrayProblems/FirstRepateingElementArray.java
@@ -0,0 +1,43 @@
+package arrayProblems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstRepateingElementArray {
+
+
+	    
+	    public static void fristRepeated(int [] arr){
+	/*
+	 * Set<Integer> set = new HashSet<>();
+	 * 
+	 * for(int num : arr){ if( set.contains(num)){ return num; } set.add(num); }
+	 * return -1;
+	 * 
+	 * }
+	 */
+	 Arrays.sort(arr);
+	    	
+	    	for( int i =0;i< arr.length; i++) {
+	    		for( int j = i+1;j < arr.length; j++) {
+	    			if(arr[i]== arr[j]) {
+	    				System.out.println(arr[i]);
+	    				return;
+	    			}
+	    		}
+	    	}
+		
+	    
+	    
+	    }    
+	    public static void main(String[] args) {
+	    int [] arr = {10,5,3,4,3,5,6};
+      
+	 fristRepeated(arr); 
+	    }
+	}
+	
+	
+
