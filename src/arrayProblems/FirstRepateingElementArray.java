1fdfdc3ff2e1535fdb17a2db7a9fbc509a4b2bdd
nehalahmeed1 <nehalahmed47@gmail.com>
2025-03-10 15:47:39 +0530

    Initial Commit

--- src/arrayProblems/FirstRepateingElementArray.java
+++ src/arrayProblems/FirstRepateingElementArray.java
@@ -7,37 +7,40 @@ import java.util.Set;
 
 public class FirstRepateingElementArray {
 
+	public static void fristRepeated(int[] arr) {
+		Arrays.sort(arr);
 
-	    
-	    public static void fristRepeated(int [] arr){
-	/*
-	 * Set<Integer> set = new HashSet<>();
-	 * 
-	 * for(int num : arr){ if( set.contains(num)){ return num; } set.add(num); }
-	 * return -1;
-	 * 
-	 * }
-	 */
-	 Arrays.sort(arr);
-	    	
-	    	for( int i =0;i< arr.length; i++) {
-	    		for( int j = i+1;j < arr.length; j++) {
-	    			if(arr[i]== arr[j]) {
-	    				System.out.println(arr[i]);
-	    				return;
-	    			}
-	    		}
-	    	}
-		
-	    
-	    
-	    }    
-	    public static void main(String[] args) {
-	    int [] arr = {10,5,3,4,3,5,6};
-      
-	 fristRepeated(arr); 
-	    }
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					System.out.println(arr[i]);
+					return;
+				}
+			}
+		}
+
 	}
-	
-	
 
+	public static int fristRepeatedSecondLogic(int[] arr) {
+
+		Set<Integer> set = new HashSet<>();
+
+		for (int num : arr) {
+			if (set.contains(num)) {
+				return num;
+			}
+			set.add(num);
+		}
+		return -1;
+
+	}
+
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 5, 3, 4, 3, 5, 6 };
+
+		/* Frist Logic */ fristRepeated(arr);
+		
+		/* Second Logic */fristRepeatedSecondLogic(arr);
+	}
+}
